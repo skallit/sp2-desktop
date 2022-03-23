2d3bd8ce2b39e91145613d470cbd82e3ecd3f8d7
VANDEW04Lo <VANDEW04Lo@lycee-ndduroc.com>
2022-03-23 16:46:15 +0100

    authentification api

---
+++ SafiRepay/Models/Api.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace SafiRepay.Models
+{
+    class Api
+    {
+        private static string _url = Properties.Resources.API_URL;
+        public static IRestResponse Post(String route, Dictionary<String, String> fields = null)
+        {
+            // The HTTP instance object where to send the request
+            RestClient clientApi = new RestClient(Api._url);
+
+            // Prepare the request
+            RestRequest request = new RestRequest(Method.POST);
+            request.Resource = route;
+            request.AddJsonBody(fields);
+
+            // Send the HTTP request and return the response
+            return clientApi.Execute(request);
+        }
+        public static void GetWithToken(String route, Dictionary<String, String> fields = null)
+        {
+
+        }
+    }
+}
--- SafiRepay/Views/AuthenticateWindow.xaml.cs
+++ SafiRepay/Views/AuthenticateWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RestSharp;
+using SafiRepay.Models;
 
 namespace SafiRepay
 {
@@ -24,11 +26,24 @@ namespace SafiRepay
         public AuthenticateWindow()
         {
             InitializeComponent();
+# if DEBUG
+            tbx_email.Text = "visiteur01@safi.com";
+            pbx_pwd.Password = "pwsio";
+# endif
         }
 
         private void Btn_Signin_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Clic sur le bouton");
+            // Create an associative array with the email and the password
+            Dictionary<String, String> userCredentials = new Dictionary<string, string>();
+            userCredentials.Add("email", tbx_email.Text);
+            userCredentials.Add("password", pbx_pwd.Password);
+
+            // Call /login route to the API
+            IRestResponse response = Api.Post("login", userCredentials);
+
+            // Print the response content into the console
+            Console.WriteLine(response.Content);
 
             ShellWindow mainWindow = new ShellWindow();
             mainWindow.Show();
